3546c16ae03b190e4685cdf0a51b51c024442d63
futagamiya <yutakanozaki@gmail.com>
2026-05-28 21:51:58 +0900

    2nd

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,13 +1,24 @@
-import { drizzle } from 'drizzle-orm/libsql';
+import { drizzle, type LibSQLDatabase } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.TURSO_DATABASE_URL) throw new Error('TURSO_DATABASE_URL is not set');
+let instance: LibSQLDatabase<typeof schema> | undefined;
 
-const client = createClient({
-	url: env.TURSO_DATABASE_URL,
-	authToken: env.TURSO_AUTH_TOKEN
+function getDb(): LibSQLDatabase<typeof schema> {
+	if (instance) return instance;
+	if (!env.TURSO_DATABASE_URL) throw new Error('TURSO_DATABASE_URL is not set');
+	const client = createClient({
+		url: env.TURSO_DATABASE_URL,
+		authToken: env.TURSO_AUTH_TOKEN
+	});
+	instance = drizzle(client, { schema });
+	return instance;
+}
+
+export const db = new Proxy({} as LibSQLDatabase<typeof schema>, {
+	get(_target, prop, receiver) {
+		const value = Reflect.get(getDb(), prop, receiver);
+		return typeof value === 'function' ? value.bind(getDb()) : value;
+	}
 });
-
-export const db = drizzle(client, { schema });
